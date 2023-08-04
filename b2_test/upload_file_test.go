41cb352fae07049e9b53c46b8fd2b80b88aa4b4c
Ben Busby <contact@benbusby.com>
2023-08-04 15:49:59 -0600

    Add b2_upload_file test

---
+++ b2_test/upload_file_test.go
@@ -0,0 +1,49 @@
+package b2
+
+import (
+	"crypto/rand"
+	"crypto/sha1"
+	"fmt"
+	. "github.com/benbusby/b2"
+	"os"
+	"reflect"
+	"testing"
+)
+
+var account Auth
+
+func init() {
+	bucketKeyID := os.Getenv("B2_TEST_KEY_ID")
+	bucketKey := os.Getenv("B2_TEST_KEY")
+
+	account, _ = AuthorizeAccount(bucketKeyID, bucketKey)
+}
+
+func TestGetUploadURL(t *testing.T) {
+	bucketID := os.Getenv("B2_TEST_BUCKET_ID")
+	info, err := account.GetUploadURL(bucketID)
+
+	if err != nil {
+		t.Fatal("Failed to get upload url from B2")
+	} else if reflect.ValueOf(info).IsZero() {
+		t.Fatal("Empty response from B2")
+	}
+}
+
+func TestUploadFile(t *testing.T) {
+	info, _ := account.GetUploadURL(os.Getenv("B2_TEST_BUCKET_ID"))
+
+	data := make([]byte, 10)
+	_, _ = rand.Read(data)
+
+	checksum := fmt.Sprintf("%x", sha1.Sum(data))
+	filename := fmt.Sprintf("%s.txt", checksum)
+
+	file, err := UploadFile(info, filename, checksum, data)
+
+	if err != nil {
+		t.Fatal("Failed to upload file to B2")
+	} else if reflect.ValueOf(file).IsZero() {
+		t.Fatal("Empty response from B2")
+	}
+}
